8cfd9c62206c6eac30daca47ffebc6a71b8bfbd6
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-11-17 02:14:37 +0100

    kanban with dragDrop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { MainViewComponent } from './pages/main-view/main-view.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MainViewComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
